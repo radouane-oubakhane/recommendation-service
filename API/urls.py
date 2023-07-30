7879e08ea445a99b16ea6e59c2c994d5ef69b6b7
Radouane Oubakhane <oubakhaneradouane@gmail.com>
2023-07-30 15:05:55 +0100

    Add debug toolbar.

--- API/urls.py
+++ API/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 # URL Configuration for API app
 urlpatterns = [
-    path('', views.get_recommendations, name='get_recommendations'),
-    path('<int:user_id>/', views.get_user_recommendations, name='get_user_recommendations'),
+    path('v1/recommendation/', views.get_recommendations, name='get_recommendations'),
+    path('v1/recommendation/<int:user_id>/', views.get_user_recommendations, name='get_user_recommendations'),
 ]
 
--- RecommendationService/urls.py
+++ RecommendationService/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/recommendation/', include('API.urls')),
+    path('api/', include('API.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
