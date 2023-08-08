a7e6e16298f5ec27c21ed79f26dce60500b3f473
Radouane Oubakhane <oubakhaneradouane@gmail.com>
2023-08-08 16:51:22 +0100

    Add Recommendation model to catalog app.

--- Catalog/models.py
+++ Catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+# recommendation model
+class Recommendation(models.Model):
+    user_id = models.IntegerField()
+    movie_id = models.IntegerField()
+    rating = models.FloatField()
+    timestamp = models.IntegerField()
+
--- Recommendation/models.py
+++ Recommendation/models.py
@@ -3,25 +3,4 @@ from django.db import models
 # Create your models here.
 
 
-# User model for recommendation app 
-class User(models.Model):
 
-    GENDER_CHOICES = [
-        'M', 'Male',
-        'F', 'Female',
-        'O', 'Other'
-    ] 
-    user_id = models.IntegerField()
-    age = models.IntegerField()
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    preferences = models.TextField()
-    favorite_movies = models.TextField()
-    favorite_directors = models.TextField()
-    favorite_actors = models.TextField()
-    saved_movies = models.TextField()
-    watched_movies = models.TextField()
-    watch_list = models.TextField()
-    
-
-    def __str__(self):
-        return self.username
