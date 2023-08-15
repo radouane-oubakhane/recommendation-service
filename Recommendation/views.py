5aab81cda7bb5ed08304717aa5c7783f5820c342
Radouane Oubakhane <oubakhaneradouane@gmail.com>
2023-08-15 16:01:58 +0100

    Add generate endpoint for generating recommendations for a specific user, a list of users, and all users

--- Recommendation/views.py
+++ Recommendation/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from Catalog.serializers import RecommendationSerializer
-from Catalog.models import Recommendation
+
 from Recommendation.tasks import run_model, run_model_for_user, run_model_for_users
 
 
@@ -14,9 +13,9 @@ from Recommendation.tasks import run_model, run_model_for_user, run_model_for_us
 
 @api_view(['GET'])
 def generate_recommendations_for_all_users(request):
+    # run the model and get the results
     run_model.delay()
-    return Response({'message': 'Recommendations generated successfully'}, status=status.HTTP_200_OK)
-
+    return Response({'message': 'Recommendations engine is running in the background in generation of recommendations for all users and it will take some time'}, status=status.HTTP_200_OK)
 
 # generate recommendations for a specific user
 
@@ -30,5 +29,4 @@ def generate_recommendations_for_user(request, user_id):
 def generate_recommendations_for_users(request, user_ids):
     run_model_for_users.delay(user_ids)
     return Response({'message': 'Recommendations generated successfully'}, status=status.HTTP_200_OK)
-
 
